1e5a687e483adf63e85688c6be4af5ecc0fd08cc
Willie <williekaufman1@gmail.com>
2023-06-29 03:24:47 -0400

    x

--- redis_utils.py
+++ redis_utils.py
@@ -11,7 +11,7 @@ def rget(key: str, *, game_id: Optional[str]) -> Optional[str]:
         raw_result = redis.get(f'{game_id}:{key}')
     return raw_result.decode('utf-8') if raw_result is not None else None
 
-def rset(key: str, value: Any, *, game_id: Optional[str], ex: Optional[int] = 3600) -> None:
+def rset(key: str, value: Any, *, game_id: Optional[str], ex: Optional[int] = 86400) -> None:
     key = f'rpoker:{key}'
     if game_id is None:
         redis.set(key, value, ex=ex)    
